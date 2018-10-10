02f7532757726846049d380ae9f1db09d25badb1
RSmetanyuk <rsmetanyuk@gmail.com>
2018-10-10 19:36:22 +0300

    made two input fields

--- src/index.js
+++ src/index.js
@@ -2,36 +2,35 @@
 
 const $tagsInputs = document.getElementsByClassName('tags-input');
 const $tagsLists = document.getElementsByClassName('tags-list');
-const tagsArr = ['1', 'super tag'];
+const tagsArrs = [[], ['1', 'super tag']];  // add default tags here
 
-for (let i = 0; i < $tagsInputs.length; i++) {
+for (let i = 0; i < $tagsInputs.length; i++) {  // adding input event listener
   $tagsInputs[i].addEventListener('keyup', ({key, target}) => {
     if (key === 'Enter' && target.value.trim()) {
       const $el = createTagElement(target.value);
-      const isElementExist = tagsArr.includes(target.value);
+      const isElementExist = tagsArrs[i].includes(target.value);
   
       if (!isElementExist) {
-        $tagsLists[0].appendChild($el);
-        tagsArr.push(target.value);
+        $tagsLists[i].appendChild($el);
+        tagsArrs[i].push(target.value);
         target.value = '';
       }
     }
   });
 };
 
-for (let i = 0; i < $tagsLists.length; i++) {
+for (let i = 0; i < $tagsLists.length; i++) {   // adding romoving event listener
   $tagsLists[i].addEventListener('click', event => {
     const {target} = event;
     const isRemoveBtn = target.classList.contains('remove-btn');
     const $foo = target.parentElement.parentElement;
     const $tag = target.parentElement;
     const removedTagName = target.parentElement.firstElementChild.innerHTML;
-    const removedTagIndex = tagsArr.indexOf(removedTagName);
+    const removedTagIndex = tagsArrs[i].indexOf(removedTagName);
   
     if (isRemoveBtn) {
-          
       $foo.removeChild($tag);
-      tagsArr.splice(removedTagIndex, 1);    
+      tagsArrs[i].splice(removedTagIndex, 1);    
     }
   });
 };
@@ -49,11 +48,14 @@ const createTagElement = content => {
 };
 
 const init = () => {
-  tagsArr.forEach(tagValue => {
-    const $el = createTagElement(tagValue);
-
-    $tagsLists[0].appendChild($el);
-  });
+  for (let i = 0; i < tagsArrs.length; i++) {
+    tagsArrs[i].forEach(tagValue => {
+      const $el = createTagElement(tagValue);
+  
+      $tagsLists[i].appendChild($el);    
+    });
+    //console.error(tagsArrs);
+  };
 };
 
 init();
