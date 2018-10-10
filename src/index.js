b1e0588353b575490f01051bdd760426a14c447d
unknown <aloudi86@gmail.com>
2018-10-10 22:19:49 +0300

    added input with default tags functionality, refactored the code to reuse event handlers on different input sorces, added css overflow and heigth-min property to prevent multiple inputs that exeed the viewpoin height of breaking the layout

--- src/index.js
+++ src/index.js
@@ -1,54 +1,72 @@
-'use strict';
+const $tagsInputSimple = document.getElementById('tags-input-simple');
+const $tagsListSimple = document.getElementById('tags-list-simple');
+const $tagsInputWithDefault = document.getElementById('tags-input-with-default');
+const $tagsListWithDefault = document.getElementById('tags-list-with-default');
+const tagsArrSimple = [];
+const tagsArrWithDefault = ['1', 'some tag',];
 
-const $tagsInput = document.getElementById('tags-input');
-const $tagsList = document.getElementById('tags-list');
-const tagsArr = ['1', 'super tag'];
-
-$tagsInput.addEventListener('keyup', ({key, target}) => {
-  if (key === 'Enter' && target.value.trim()) {
+const addTagEventHandler = (event, outputElement, localArr) => {
+  const { key, target } = event;
+  
+  if (key === 'Enter' && target.value) {
     const $el = createTagElement(target.value);
-    const isElementExist = tagsArr.includes(target.value);
+    const doesElementExist = localArr.includes(target.value);
 
-    if (!isElementExist) {
-      $tagsList.appendChild($el);
-      tagsArr.push(target.value);
+    if (!doesElementExist) {
+      outputElement.appendChild($el);
+      localArr.push(target.value);
       target.value = '';
     }
   }
-});
+};
 
-$tagsList.addEventListener('click', event => {
-  const {target} = event;
+const removeTagEventHandler = (event, localArr) => {
+  const { target } = event;
   const isRemoveBtn = target.classList.contains('remove-btn');
-  const $foo = target.parentElement.parentElement;
+  const $parentElement = target.parentElement.parentElement;
   const $tag = target.parentElement;
 
   if (isRemoveBtn) {
-    $foo.removeChild($tag);
-
-    // TODO: remove element from array
-    // tagsArr.splice();
+    $parentElement.removeChild($tag);
+    localArr.splice(tagsArrSimple.indexOf($tag.children[0].innerText), 1);
   }
-});
+};
 
 const createTagElement = content => {
   const $li = document.createElement('li');
-
   $li.className = 'tags-list-item';
   $li.innerHTML = `
     <span class="tag-name">${content}</span>
     <span class="remove-btn">x</span>
   `;
-
   return $li;
 };
 
 const init = () => {
-  tagsArr.forEach(tagValue => {
-    const $el = createTagElement(tagValue);
+  
+  const populateDefaultTags = () => {
+    tagsArrWithDefault.forEach(tagValue => {
+      const $el = createTagElement(tagValue);
+      $tagsListWithDefault.appendChild($el);
+    });
+  };
 
-    $tagsList.appendChild($el);
+  populateDefaultTags();
+
+  $tagsInputSimple.addEventListener('keyup', event => {
+    addTagEventHandler(event, $tagsListSimple, tagsArrSimple);
   });
+  $tagsInputWithDefault.addEventListener('keyup', event => {
+    addTagEventHandler(event, $tagsListWithDefault, tagsArrWithDefault);
+  });
+  $tagsListSimple.addEventListener('click', event => {
+    removeTagEventHandler(event, tagsArrSimple);
+  });
+  $tagsListWithDefault.addEventListener('click', event => {
+    removeTagEventHandler(event, tagsArrWithDefault);
+  });
+
 };
 
 init();
+
