f17fd9d082f5e06fcdda85077a3e671fd7b2715d
dovganych <misha.dovganych@gmail.com>
2018-10-12 00:30:32 +0300

    fixed and added new feature

--- src/index.js
+++ src/index.js
@@ -1,54 +1,107 @@
 'use strict';
 
-const $tagsInput = document.getElementById('tags-input');
-const $tagsList = document.getElementById('tags-list');
-const tagsArr = ['1', 'super tag'];
-
-$tagsInput.addEventListener('keyup', ({key, target}) => {
-  if (key === 'Enter' && target.value.trim()) {
-    const $el = createTagElement(target.value);
-    const isElementExist = tagsArr.includes(target.value);
-
-    if (!isElementExist) {
-      $tagsList.appendChild($el);
-      tagsArr.push(target.value);
-      target.value = '';
-    }
+class TagsInput {
+  constructor(parentId, headingText, defaultArr) {
+    this.$parent = document.getElementById(parentId);
+    this.headingText = headingText;
+    this.tagsArr = this.defaultTags(defaultArr);
+    this.constructor.counter = (this.constructor.counter || 0) + 1;
+    this._id = this.constructor.counter;
   }
-});
 
-$tagsList.addEventListener('click', event => {
-  const {target} = event;
-  const isRemoveBtn = target.classList.contains('remove-btn');
-  const $foo = target.parentElement.parentElement;
-  const $tag = target.parentElement;
-
-  if (isRemoveBtn) {
-    $foo.removeChild($tag);
-
-    // TODO: remove element from array
-    // tagsArr.splice();
+  get id() {
+    return this._id;
   }
-});
 
-const createTagElement = content => {
-  const $li = document.createElement('li');
+  createTagsInput(){
+    const $input = document.createElement('input');
+    $input.className = 'tags-input';
+    $input.id = 'tags-input-'+ this.id;
+    $input.type = 'text';
+    $input.placeholder='type your tags...';
+    return $input;
+  }
 
-  $li.className = 'tags-list-item';
-  $li.innerHTML = `
-    <span class="tag-name">${content}</span>
-    <span class="remove-btn">x</span>
-  `;
+  createTagsList(){
+    const $ul = document.createElement('ul');
+    $ul.className = 'tags-list';
+    $ul.id = 'tags-list-'+ this.id;
+    return $ul;
+  }
 
-  return $li;
-};
+  createHeading() {
+    const $h2 = document.createElement('h2');
+    $h2.className = 'title-level-2';
+    $h2.innerHTML = this.headingText;
+    return $h2;
+  }
 
-const init = () => {
-  tagsArr.forEach(tagValue => {
-    const $el = createTagElement(tagValue);
+  mountElement(){
+    this.$tagsInput = this.createTagsInput();
+    this.$tagsList = this.createTagsList();
+    this.$parent.appendChild(this.createHeading());
+    this.$parent.appendChild(this.$tagsInput);
+    this.$parent.appendChild(this.$tagsList);
+    this.init();
+  }
 
-    $tagsList.appendChild($el);
-  });
-};
+  init () {
+    this.fillTagsList();
+    this.listeningInput();
+    this.listeningRemoveBtn();
+  }
 
-init();
+  defaultTags (defaultArr) {
+    return defaultArr !== undefined ? [...new Set(defaultArr)] : [];
+  }
+
+  createTagElement (content) {
+    const $li = document.createElement('li');
+    $li.className = 'tags-list-item';
+    $li.innerHTML = `
+      <span class="tag-name">${content}</span>
+      <span class="remove-btn">x</span>
+    `;
+    return $li;
+  }
+
+  fillTagsList () {
+    this.tagsArr.forEach(tagValue => {
+      const $el = this.createTagElement(tagValue);
+      this.$tagsList.appendChild($el);
+    });
+  }
+
+  listeningInput () {
+    this.$tagsInput.addEventListener('keyup', ({key, target}) => {
+      if (key === 'Enter' && target.value.trim()) {
+        const $el = this.createTagElement(target.value),
+          isElementExist = this.tagsArr.includes(target.value);
+        if (!isElementExist) {
+          this.$tagsList.appendChild($el);
+          this.tagsArr.push(target.value);
+          target.value = '';
+        }
+      }
+    });
+  }
+
+  listeningRemoveBtn () {
+    this.$tagsList.addEventListener('click', event => {
+      const {target} = event,
+        isRemoveBtn = target.classList.contains('remove-btn');
+      if (isRemoveBtn) {
+        const $tagValue = target.previousElementSibling.textContent;
+        this.$tagsList.removeChild(target.parentElement);
+        this.tagsArr.splice(this.tagsArr.indexOf($tagValue),1);
+      }
+    });
+  }
+}
+
+//Example of using
+
+const firstInput = new TagsInput('app','Simple Input');
+firstInput.mountElement();
+const secondInput = new TagsInput('app','Input with default tags:', ['tag1', 'some awesome tag']);
+secondInput.mountElement();
