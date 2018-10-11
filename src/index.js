a67a782c1ae7b6dde0edb43f4e5bf3e0cd850b9f
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2018-10-12 00:16:25 +0300

    UPDATE app tag-input. Add users confirm brfore remove tags

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@
 const $tagsInputFirst = document.getElementById('tags-input-1');
 const $tagsInputSecond = document.getElementById('tags-input-2');
 const $tagsList = document.getElementsByClassName('tags-list');
-const tagsArrFirst = [];
-const tagsArrSecond = ['tag1', 'some awesome tag'];
+const $modal = document.getElementById('question'); // modal window
+const $btnsInModal = $modal.getElementsByClassName('btn');  // buttons Remove and Close in modal window
+const $btnCloseInModal = document.getElementById('close-btn');  // span-button to close modal window
+const tagsArrFirst = [];  // array for first input
+const tagsArrSecond = ['tag1', 'some awesome tag'];   // array for second input
 
-$tagsInputFirst.addEventListener('keyup', ({key, target}) => {
+$tagsInputFirst.addEventListener('keyup', ({key, target}) => {  // event for first input
   if (key === 'Enter' && target.value.trim()) {
     const $el = createTagElement(target.value);
     const isElementExist = tagsArrFirst.includes(target.value);
@@ -19,7 +22,7 @@ $tagsInputFirst.addEventListener('keyup', ({key, target}) => {
   }
 });
 
-$tagsInputSecond.addEventListener('keyup', ({key, target}) => {
+$tagsInputSecond.addEventListener('keyup', ({key, target}) => {  // event for second input
   if (key === 'Enter' && target.value.trim()) {
     const $el = createTagElement(target.value);
     const isElementExist = tagsArrSecond.includes(target.value);
@@ -32,35 +35,41 @@ $tagsInputSecond.addEventListener('keyup', ({key, target}) => {
   }
 });
 
-
-
-$tagsList[0].addEventListener('click', event => {
+$tagsList[0].addEventListener('click', event => {  // event for first tags
   const {target} = event;
   const isRemoveBtn = target.classList.contains('remove-btn');
   const $foo = target.parentElement.parentElement;
-  const $tag = target.parentElement;
-  const indexOfItem = tagsArrFirst.indexOf(target.value);
-
+  const $tag = target.parentElement;  
+  
   if (isRemoveBtn) {
-    $foo.removeChild($tag);
-    tagsArrFirst.splice(indexOfItem,1);
+    const tagName = $tag.getElementsByClassName('tag-name')[0].innerHTML; // tag's name 
+    const indexOfItem = tagsArrFirst.indexOf(tagName);  // tag's index in array
+    questionToggle(tagName);  // open modal window
+    $btnsInModal[0].addEventListener('click', () => { // event for Remove button in modal
+      $foo.removeChild($tag); // remove selected(clicked) tag
+      tagsArrFirst.splice(indexOfItem,1); // remove tag's name from array
+      questionToggle();   // close modal window
+    });    
   }
 });
 
-$tagsList[1].addEventListener('click', event => {
+$tagsList[1].addEventListener('click', event => {  // event for second tags
   const {target} = event;
   const isRemoveBtn = target.classList.contains('remove-btn');
   const $foo = target.parentElement.parentElement;
-  const $tag = target.parentElement;
-  const indexOfItem = tagsArrSecond.indexOf(target.value);
-
+  const $tag = target.parentElement;  
+  
   if (isRemoveBtn) {
-    $foo.removeChild($tag);
-    tagsArrSecond.splice(indexOfItem,1);
+    const tagName = $tag.getElementsByClassName('tag-name')[0].innerHTML; // tag's name
+    const indexOfItem = tagsArrSecond.indexOf(tagName);  // tag's index in array
+    questionToggle(tagName);  // open modal window
+    $btnsInModal[0].addEventListener('click', () => { // event for Remove button in modal
+      $foo.removeChild($tag); // remove selected(clicked) tag
+      tagsArrSecond.splice(indexOfItem,1); // remove tag's name from array
+      questionToggle(); // close modal window
+    });
   }
 });
-
-
 
 const createTagElement = content => {
   const $li = document.createElement('li');
@@ -84,3 +93,22 @@ const init = ($tag, arr) => {
 
 init($tagsList[0], tagsArrFirst);
 init($tagsList[1], tagsArrSecond);
+
+$btnsInModal[1].addEventListener('click', () => {  // event for button Cancel in modal wimdow
+  questionToggle('');
+});
+
+$btnCloseInModal.addEventListener('click', () => {  // event for span-button Close in modal wimdow
+  questionToggle('');
+});
+
+const questionToggle = text => {    //  function to open/close modal window.   
+  const $p = document.getElementById('alert-msg');
+  
+  $p.innerHTML = `
+  <span>Tag "${text}" will be remove.<br>Are you sure?</span>  
+  `;  //  created question for user
+  $modal.className === 'hidden' ? $modal.className = 'visible' : $modal.className = 'hidden';
+};
+
+
