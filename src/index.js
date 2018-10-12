0105f2aeb196b03d2a5775ddeb28d912ffc7db94
JuliaSavych <julliasavych@gmail.com>
2018-10-12 16:44:54 +0300

    defaul input

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 'use strict';
 
-const $tagsInput = document.getElementById('tags-input');
+const $tagsInput = document.getElementById('tags-input-1');
 const $tagsList = document.getElementById('tags-list');
-const tagsArr = ['1', 'super tag'];
+const $tagsInputDefault = document.getElementById('tags-input-2');
+const $tagsListDefault = document.getElementById('tags-list-default');
+const tagsArr = [];
+const tagsArrDefault = ['1', 'super tag', 'some defaul tag', 'lol'];
+
 
 $tagsInput.addEventListener('keyup', ({key, target}) => {
   if (key === 'Enter' && target.value.trim()) {
@@ -12,6 +16,19 @@ $tagsInput.addEventListener('keyup', ({key, target}) => {
     if (!isElementExist) {
       $tagsList.appendChild($el);
       tagsArr.push(target.value);
+      target.value = '';
+    }
+  }
+});
+
+$tagsInputDefault.addEventListener('keyup', ({key, target}) => {
+  if (key === 'Enter' && target.value.trim()) {
+    const $def = createTagElement(target.value);
+    const isElementExist = tagsArrDefault.includes(target.value);
+
+    if (!isElementExist) {
+      $tagsListDefault.appendChild($def);
+      tagsArrDefault.push(target.value);
       target.value = '';
     }
   }
@@ -27,7 +44,21 @@ $tagsList.addEventListener('click', event => {
     $foo.removeChild($tag);
 
     // TODO: remove element from array
-    // tagsArr.splice();
+    tagsArr.splice(tagsArr.indexOf($tag.firstElementChild.textContent), 1);
+  }
+});
+
+$tagsListDefault.addEventListener('click', event => {
+  const {target} = event;
+  const isRemoveBtn = target.classList.contains('remove-btn');
+  const $foo = target.parentElement.parentElement;
+  const $tag = target.parentElement;
+
+  if (isRemoveBtn) {
+    $foo.removeChild($tag);
+
+    // TODO: remove element from array
+    tagsArrDefault.splice(tagsArrDefault.indexOf($tag.firstElementChild.textContent), 1);
   }
 });
 
@@ -48,6 +79,11 @@ const init = () => {
     const $el = createTagElement(tagValue);
 
     $tagsList.appendChild($el);
+  });
+  tagsArrDefault.forEach(tagValue => {
+    const $def = createTagElement(tagValue);
+
+    $tagsListDefault.appendChild($def);
   });
 };
 
